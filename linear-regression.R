72c7708d38248829b3cfa6fa58b95dbd8b844fe0
Dyego Rameh <38622738+ramehx@users.noreply.github.com>
2018-09-27 21:11:30 -0300

    Update linear-regression.R

--- linear-regression.R
+++ linear-regression.R
@@ -2,3 +2,7 @@
 ggplot(data = bdims, aes(x = hgt, y = wgt)) + 
   geom_point() + 
   geom_smooth(method = "lm", se = FALSE)
+
+
+# Linear model for weight as a function of height
+lm(hgt ~ wgt, data = bdims)
